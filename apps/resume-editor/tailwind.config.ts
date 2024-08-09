430d2aa9a3814c68f0addd97922b4cad5bfab639
qiyuhang <qiyuhang@zhixue.com>
2024-08-09 13:45:03 +0800

    feat: preloader components

--- apps/resume-editor/tailwind.config.ts
+++ apps/resume-editor/tailwind.config.ts
@@ -7,14 +7,15 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
     },
   },
   darkMode: 'class',
@@ -46,7 +47,6 @@ const config: Config = {
           layout: {}, // dark theme layout tokens
           colors: {}, // dark theme colors
         },
-        // ... custom themes
       },
     }),
   ],
