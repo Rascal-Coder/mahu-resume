320db7e34ceb755a24e7f2fee52ed49d6e874ff7
qiyuhang <qiyuhang@zhixue.com>
2024-08-14 11:55:46 +0800

    feat: BackgroundBeams component

--- apps/resume-editor/src/app/page.tsx
+++ apps/resume-editor/src/app/page.tsx
@@ -1,19 +1,22 @@
 'use client';
 // import { useLayoutEffect, useState } from 'react';
 
+import BackgroundBeams from '@/components/other/BackgroundBeams';
 import PreLoader from '@/components/other/PreLoader';
 
 import { Hero } from '@/app/home/Hero';
 import { Steps } from '@/app/home/Steps';
-
 export default function Home() {
   return (
-    <main className="h-screen overflow-y-scroll">
-      <PreLoader></PreLoader>
-      <main className="max-w-screen-2xl py-8 mx-auto  bg-dot px-8 text-gray-900 lg:px-12">
-        <Hero />
-        <Steps />
+    <>
+      <BackgroundBeams />
+      <main className="h-screen overflow-y-scroll">
+        <PreLoader></PreLoader>
+        <main className="max-w-screen-2xl py-8 mx-auto  bg-dot px-8 text-gray-900 lg:px-12">
+          <Hero />
+          <Steps />
+        </main>
       </main>
-    </main>
+    </>
   );
 }
